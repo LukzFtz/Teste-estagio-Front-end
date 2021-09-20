a3b313cc433d8b610da8dd2560fc4b399350a5f0
LukzFtz <luke.ftzp@gmail.com>
2021-09-20 17:26:06 -0300

    final code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -19,18 +19,18 @@ export default function Navbar() {
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div className=" d-none d-md-block col-md-5" />
-                    <div class="d-none d-md-block col-md-4 " id="navbarTogglerDemo02">
+                    <div class="d-none d-lg-block col-md-4 " id="navbarTogglerDemo02">
                         <div class="row align-items-center navbar-nav me-auto mb-2 mb-lg-0 ">
-                            <li class="nav-item col-md-4">
+                            <li class="nav-item col-md-5 col-xl-5">
                                 <a class="nav-link fs-4 display-6 text-light " aria-current="page" href="#secondPageMainDiv">Funcionalidades
                                 </a>
                             </li>
-                            <li class="nav-item col-md-3">
+                            <li class="nav-item col-md-5 col-xl-4">
                                 <a class="nav-link fs-4 display-6 text-light" href="#contactPage">
                                     contato
                                 </a>
                             </li>
-                            <li class="nav-item col-md-2 ">
+                            <li class="nav-item col-md-2 col-xl-2">
                                 <a class="nav-link fs-4 display-6 text-light" href="#">
                                     <div className="col-md-10">
                                         <Facebook />
@@ -39,7 +39,7 @@ export default function Navbar() {
                             </li>
                         </div>
                     </div>
-                    <div class=" d-md-none collapse navbar-collapse col-md-4" id="navbarTogglerDemo02">
+                    <div class=" d-lg-none collapse navbar-collapse col-md-4" id="navbarTogglerDemo02">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0" style={{ backgroundColor: "#572883" }}>
                             <li class="nav-item">
                                 <a class="nav-link fs-4 display-6 text-light " aria-current="page" href="#secondPageMainDiv">Funcionalidades
